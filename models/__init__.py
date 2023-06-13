cffa432466725e04d3a5d1b6d9a2afc4039a9b08
Lewis lou <weilou@link.cuhk.edu.cn>
2023-06-13 10:23:09 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -6,5 +6,5 @@ Created on Sun Mar 20 14:23:55 2022
 @author: jma
 """
 
-from .unetr2d import UNETR2D
-from .swin_unetr import SwinUNETR
+#from .unetr2d import UNETR2D
+#from .swin_unetr import SwinUNETR
